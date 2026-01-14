4073f3dc63189d8ae9927a30990ef8090a82ad01
MyTHOMASTHOMAS <thomasthomasz2xz@gmail.com>
2026-01-14 16:20:07 +0400

    FEAT(Header - Categorie): Ajout du lien vers la page des category dans le header, protection des routes category pour l'admin

--- src/Controller/Api/CategoryController.php
+++ src/Controller/Api/CategoryController.php
@@ -10,7 +10,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[isGranted('ROLE_ADMIN')]
 #[Route('/api/category')]
 class CategoryController extends AbstractController
 {
